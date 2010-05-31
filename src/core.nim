90b66b924747ef2b8d401750424d8ec70c2eed7d
dom96 <dominikpicheta@googlemail.com>
2010-05-31 22:19:56 +0100

    Less includes, and importing modules works.

--- src/core.nim
+++ src/core.nim
@@ -2,18 +2,25 @@
 # 31 May 2010
 
 # core - Implements commands like 'print'
-import unity
 
-proc interpretQuotation*(quot: PType, dataStack: var TStack, vars: var PType)
+proc interpretQuotation*(quot: PType, dataStack: var TStack, vars, gvars: var PType)
+proc loadModules(modules: seq[string], vars, gvars: var PType)
 
-proc command*(cmnd: string, dataStack: var TStack, vars: var PType) =
+proc command*(cmnd: string, dataStack: var TStack, vars, gvars: var PType) =
   case cmnd
   of "print":
     var first = dataStack.pop()
     echo($first)
   of "call":
     var first = dataStack.pop()
-    interpretQuotation(first, dataStack, vars)
+    interpretQuotation(first, dataStack, vars, gvars)
+  of "import":
+    var first = dataStack.pop()
+    if first.kind == ntString:
+      loadModules(@[first.value], vars, gvars)
+      
+
+    
   else:
     var tVar = vars.getVar(cmnd)
     if tVar == nil:
@@ -22,7 +29,7 @@ proc command*(cmnd: string, dataStack: var TStack, vars: var PType) =
     
     # TODO: Functions
 
-proc interpretQuotation*(quot: PType, dataStack: var TStack, vars: var PType) =
+proc interpretQuotation*(quot: PType, dataStack: var TStack, vars, gvars: var PType) =
   if quot.kind != ntQuot:
     raise newException(EInvalidValue, "Error: Argument given is not a quotation")
   
@@ -31,4 +38,25 @@ proc interpretQuotation*(quot: PType, dataStack: var TStack, vars: var PType) =
     of ntInt, ntFloat, ntString, ntList, ntQuot, ntDict, ntNil:
       dataStack.push(item)
     of ntCmnd:
-      command(item.value, dataStack, vars)
+      command(item.value, dataStack, vars, gvars)
+
+proc loadModules(modules: seq[string], vars, gvars: var PType) =
+  var paths = gvars.getVar("__path__")
+  if paths != nil:
+    for path in items(paths.lValue):
+      for module in items(modules):
+        var file = readFile(path.value / module & ".nael")
+        if file != nil:
+          var locals = newVariables()
+          var globals = newVariables()
+          globals.addStandard()
+          var moduleStack = newStack(200)
+          
+          var ast = parse(file)
+          interpret(ast, moduleStack, locals, globals)
+          
+          var moduleList = newList(@[newString(module), locals, globals]) # [name, {locals}, {globals}]
+          var modulesVar = gvars.getVar("__modules__")
+          modulesVar.lValue.add(moduleList)
+        else:
+          echo("File not found.. ", path.value / module & ".nael")
--- src/interpreter.nim
+++ src/interpreter.nim
@@ -2,13 +2,181 @@
 # 30 May 2010
 
 # Interpreter - Executes the AST
-import unity, core, strutils
+import parser, strutils, os
 
 type
-  ERuntimeError* = object of EBase
+  TDict* = seq[tuple[name: string, value: PType]]
 
-var dataStack = newStack(200)
-var vars = newVariables() # global variables
+  TTypes* = enum
+    ntInt,
+    ntFloat,
+    ntString,
+    ntList,
+    ntQuot,
+    ntDict,
+    ntNil,
+    ntCmnd # Exclusive to quotations.
+    
+  PType* = ref TType
+  TType* = object
+    case kind*: TTypes
+    of ntInt:
+      iValue*: int64
+    of ntFloat:
+      fValue*: float64
+    of ntString, ntCmnd:
+      value*: string
+    of ntList, ntQuot:
+      lValue*: seq[PType]
+    of ntDict:
+      dValue*: TDict
+    of ntNil: nil
+      
+  TStack* = tuple[stack: seq[PType], limit: int]
+  
+  ERuntimeError* = object of EBase
+  EVar* = object of EBase
+
+# Stack
+proc `$`*(item: PType): string =
+  result = ""
+
+  case item.kind
+  of ntInt:
+    return $item.iValue
+  of ntFloat:
+    return $item.fValue
+  of ntString, ntCmnd:
+    return item.value
+  of ntList:
+    result.add("[")
+    for i in 0 .. len(item.lValue)-1:
+      result.add($item.lValue[i])
+      if i < len(item.lValue)-1:
+        result.add(", ")
+    result.add("]")
+      
+  of ntQuot:
+    result.add("(")
+    for i in 0 .. len(item.lValue)-1:
+      result.add($item.lValue[i])
+      if i < len(item.lValue)-1:
+        result.add(", ")
+    result.add(")")
+  of ntDict:
+    result.add("__dict__")
+  of ntNil:
+    result.add("nil")
+
+proc newStack*(limit: int): TStack =
+  result.stack = @[]
+  result.limit = limit
+
+proc push*(stack: var TStack, item: PType) =
+  if stack.stack.len() >= stack.limit:
+    raise newException(EOverflow, "Error: Stack overflow")
+    
+  stack.stack.add(item)
+
+proc pop*(stack: var TStack): PType =
+  if stack.stack.len() < 1:
+    raise newException(EOverflow, "Error: Stack underflow")
+    
+  return stack.stack.pop()
+
+proc newInt*(value: int64): PType =
+  new(result)
+  result.kind = ntInt
+  result.iValue = value
+  
+proc newFloat*(value: float64): PType =
+  new(result)
+  result.kind = ntFloat
+  result.fValue = value
+
+proc newString*(value: string): PType =
+  new(result)
+  result.kind = ntString
+  result.value = value
+
+proc newList*(value: seq[PType]): PType =
+  new(result)
+  result.kind = ntList
+  result.lValue = value
+
+proc newCmnd*(value: string): PType =
+  new(result)
+  result.kind = ntCmnd
+  result.value = value
+
+proc newVariables*(): PType =
+  new(result)
+  result.kind = ntDict
+  result.dValue = @[]
+  
+proc addStandard*(vars: var PType) =
+  var appDir = newString(os.getApplicationDir())
+  var pathVar = newList(@[appDir])
+  vars.dValue.add(("__path__", pathVar))
+  
+  var modulesVar: PType # [["name", {locals}, {globals}], ...]
+  modulesVar = newList(@[])
+  vars.dValue.add(("__modules__", modulesVar))
+  
+proc getVar*(vars: var PType, name: string): PType =
+  if vars.kind != ntDict:
+    raise newException(EInvalidValue, "Error: The variable list needs to be a dict.")
+
+  for vname, value in items(vars.dValue):
+    if vname == name:
+      return value
+      
+  return nil
+
+proc getVarIndex*(vars: var PType, name: string): int =
+  if vars.kind != ntDict:
+    raise newException(EInvalidValue, "Error: The variable list needs to be a dict.")
+
+  for i in 0 .. len(vars.dValue)-1:
+    if vars.dValue[i][0] == name:
+      return i
+      
+  return -1
+  
+proc declVar*(vars: var PType, name: string) =
+  # Declares a variable
+
+  # TODO: Check if the name contains illegal characters.
+  if vars.kind != ntDict:
+    raise newException(EInvalidValue, "Error: The variable list needs to be a dict.")
+
+  if getVarIndex(vars, name) != -1:
+    raise newException(EVar, "Error: $1 is already declared." % [name])
+
+  var theVar: PType
+  new(theVar)
+  theVar.kind = ntNil
+
+  vars.dValue.add((name, theVar))
+  
+proc setVar*(vars: var PType, name: string, theVar: PType) =
+  # Sets a variables value.
+  if vars.kind != ntDict:
+    raise newException(EInvalidValue, "Error: The variable list needs to be a dict.")
+  
+  var varIndex = vars.getVarIndex(name)
+  if varIndex == -1:
+    raise newException(EVar, "Error: $1 is not declared." % [name])
+    
+  vars.dValue[varIndex][1] = theVar
+  
+proc remVar*(vars: var PType, name: string) =
+  if vars.kind != ntDict:
+    raise newException(EInvalidValue, "Error: The variable list needs to be a dict.")
+
+  for i in 0 .. len(vars.dValue)-1:
+    if vars.dValue[i][0] == name:
+      vars.dValue.del(i)
 
 proc toPType(item: PNaelNode): PType =
   ## Converts a PNaelNode of any kind. Into a corresponding PType
@@ -44,11 +212,13 @@ proc toPType(item: PNaelNode): PType =
       of nnkIntLit, nnkStringLit, nnkFloatLit, nnkListLit, nnkQuotLit:
         result.lValue.add(toPType(node))
 
-proc interpret*(ast: seq[PNaelNode]) =
+proc command*(cmnd: string, dataStack: var TStack, vars, gvars: var PType) # from core.nim
+
+proc interpret*(ast: seq[PNaelNode], dataStack: var TStack, vars, gvars: var PType) =
   for node in items(ast):
     case node.kind
     of nnkCommand:
-      command(node.value, dataStack, vars)
+      command(node.value, dataStack, vars, gvars)
     of nnkIntLit, nnkStringLit, nnkFloatLit, nnkListLit, nnkQuotLit:
       dataStack.push(toPType(node))
     of nnkVarDeclar:
@@ -58,10 +228,17 @@ proc interpret*(ast: seq[PNaelNode]) =
     
     else:
       #
-      
+
+include core # I know this is kind of bad... but i want to have all those
+             # commands in a seperate module.
+
+var dataStack = newStack(200)
+var vars = newVariables() # 'main' variables(They act as both local and global)
+vars.addStandard()
+
 when isMainModule:
-  var ast = parse("x let, x \"hai\" =, x print")
-  interpret(ast)
+  var ast = parse("\"hello\" import")
+  interpret(ast, dataStack, vars, vars)
   
   if dataStack.stack.len() > 0:
     var result = "stack["
